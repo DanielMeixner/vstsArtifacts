4c721d838dcf6a26a12bb3832612e37a7eaa2ab2
Daniel Meixner <Daniel Meixner>
2016-09-29 12:42:50 +0200

    tmpkey

--- sendCloud/sendCloudToDevice.js
+++ sendCloud/sendCloudToDevice.js
@@ -2,7 +2,7 @@
 
 var Client = require('azure-iothub').Client;
 var Message = require('azure-iot-common').Message;
-var connectionString = 'connstring';
+var connectionString = 'HostName=DmxIoTHub.azure-devices.net;SharedAccessKeyName=tmppol;SharedAccessKey=6x5MgzExXlQKNx+inTSBHq3aK1QC8+O7R3Zt37JYlOM=';
 var targetDevice = 'mydevice1';
 
 var serviceClient = Client.fromConnectionString(connectionString);

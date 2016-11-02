58d63ded8475d9369d01b3ac11e32a4dced5ee19
Daniel Meixner <Daniel Meixner>
2016-11-02 13:20:30 +0100

    string

--- sendCloud/sendCloudToDevice.js
+++ sendCloud/sendCloudToDevice.js
@@ -5,7 +5,7 @@
 var Client = require('azure-iothub').Client;
 var Message = require('azure-iot-common').Message;
 
-var connectionString = 'HostName=DmxIoTHub.azure-devices.net;DeviceId=Device31-7ce4a850;SharedAccessKey=f9xOhG21zpebXsRU+8lyYgIXZAKsf0eB0t1HMSg6tnE=';
+var connectionString = 'f9xOhG21zpebXsRU+8lyYgIXZAKsf0eB0t1HMSg6tnE=';
 var targetDevice = 'Device31-7ce4a850';
 
 var serviceClient = Client.fromConnectionString(connectionString);
